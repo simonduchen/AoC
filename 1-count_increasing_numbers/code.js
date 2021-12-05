aafdb27a3287da70fb8f807cbcb353d7ba94d64a
Simon Duchen <sduchen97@gmail.com>
2021-12-05 22:31:18 +0100

    first day done

--- 1-count_increasing_numbers/code.js
+++ 1-count_increasing_numbers/code.js
@@ -2,32 +2,56 @@ import fs from "fs";
 import readline from "readline";
 
 
-const values = readline.createInterface({
-    input : fs.createReadStream("./input.txt"),
-    output : process.stdout,
-    terminal: false
-});
 
-let currentValue = null;
+async function getData() {
 
-async function countLines() {
-    let c = 0;
-
-    for await (const value of values) {
-        if(currentValue !== null) {
-            value >= currentValue ? c++ : c=c;
-            console.log(currentValue, " | ", value, value > currentValue ? "-- Increase" : "---- Decrease", c);
-        }
-
-        currentValue = value;
-    }
+    const lines = readline.createInterface({
+        input : fs.createReadStream("./input.txt"),
+        output : process.stdout,
+        terminal: false
+    });
     
+    let array = [];
+
+    for await (let line of lines) {
+        array.push(Number(line));
+    }
+
+    return array;
+}
+
+async function concatData(array) {
+    
+    return await array.map((measurement, index) => {
+        if(index + 2 <= array.length)
+            return measurement + array[index + 1] + array[index + 2]
+    });
+
+}
+
+async function countIncreases(array) {
+    
+    let c = 0;
+    let currentMeasurement = null;
+
+    await array.forEach(measurement => {
+        measurement > currentMeasurement && 
+        currentMeasurement !== null ? c++ : c+=0;
+        
+        currentMeasurement = measurement;
+    });
     
     return c;
 }
 
-countLines().then((result) => console.log("RESULT:", result));
 
+getData()
+.then( data => countIncreases(data)
+.then((result) => console.log("RESULT PART 1:", result)));
+
+getData().then(data => concatData(data)
+.then(concatedData => countIncreases(concatedData)
+.then(result => console.log("RESULT PART 2:", result))));
 
 
 
